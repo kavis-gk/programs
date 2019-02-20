03fb8bda2e70e68ca1a981543a1fbb70450944c3
kavis-gk <43002162+kavis-gk@users.noreply.github.com>
2019-02-20 14:39:50 +0530

    Create sum of arithmetic progression.c

---
+++ sum of arithmetic progression.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void) 
+
+{
+
+int k,b,c,sum=0,a=0,i;
+
+printf("\n enter the no of terms");
+
+scanf("%d",&k);
+
+printf("enter the difference");
+
+scanf("%d",&b);
+
+printf("\n enter the first number");
+
+scanf("%d",&c);
+
+for(i=0;i<k;i++)
+
+{
+    
+a=a+d;
+ 
+   sum=sum+a;
+ 
+   
+}
+
+printf("%d",sum);
+
+ return 0;
+
+}
