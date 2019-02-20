62451225c18cbe387bb93cd79b81ab7eedbeef3f
kavis-gk <43002162+kavis-gk@users.noreply.github.com>
2019-02-20 14:10:22 +0530

    Update even numbers between two numbers.c

--- even numbers between two numbers.c
+++ even numbers between two numbers.c
@@ -1,12 +1,15 @@
-#include<stdio.h>
-int main()
-{
-int c,d,e;
-printf("enter the two limits");
-scanf("%d%d",&c,&d);
-for(e=c;e<d;e++)
-{
-if((e)%2==0)
-printf("%d\n",e);
-}
+#include <stdio.h>
+
+int main() {
+	int c,d,e;
+	printf("enter the two numbers : ");
+	scanf("%d %d",&c,&d);
+	for(e=c;e<d;e++)
+	{
+	    if(e%2==0)
+	    {
+	        printf("%d ",e);
+	    }
+	}
+	return 0;
 }
